6be18afbdd6620142dbf0fb32d9d131ac89c5411
Stealth <100754581+mikekks@users.noreply.github.com>
2022-11-11 01:57:26 +0900

    Update token.sol

--- token.sol
+++ token.sol
@@ -7,23 +7,30 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 contract token is ERC1155 {
     
     uint256 public constant MAIN = 0;
-    uint256 public constant SUB = 1;
-    uint256 public constant SONGNFT = 2;
+
+    struct MushnSpore{
+        uint256 tokenRate;
+        bool staking;
+    }
     
     mapping (uint256 => string) private _uris;
-    mapping (uint256 => uint256) private tokenRate;
     mapping (uint256 => uint256) private season;
+
+    mapping (uint256 => MushnSpore) private NFTinfo;
+    mapping (uint => uint) public stealer;
 
     address owner;
     address public stakeContract;
     uint256 public currentSeason;
+    uint256 totalSpore;
+    uint256 totalMush;
 
     constructor() public ERC1155("https://bafybeidobelazh63dhafch3yel6hjiw5rxgan7jz2oh7vc2fk2s6suielq.ipfs.dweb.link/{id}.json") {
         _mint(msg.sender, MAIN, 10e18, "");
-        _mint(msg.sender, SUB, 10e18, "");
-        _mint(msg.sender, SONGNFT, 1, "");
         owner = msg.sender;
         currentSeason = 1;
+        totalSpore = 0;
+        totalMush = 0;
     }
 
     modifier onlyOwner{
@@ -46,20 +53,71 @@ contract token is ERC1155 {
 
     function setStakeContract(address _contract) public onlyOwner {
         stakeContract = _contract;
-        //safeTransferFrom(owner, _contract, 1, 10e17, "0x00");
+    }
+
+    // need onlyOwner
+    function mintSpore(address _account, uint _tokenId, string memory _tokenuri, uint256 _tokenRate) public {
+        _mint(_account, _tokenId, 1, "");
+        setTokenUri(_tokenId, _tokenuri, 0);
+
+        NFTinfo[_tokenId].tokenRate = _tokenRate;
+        NFTinfo[_tokenId].staking = false;
+
+        season[_tokenId] = currentSeason;
+        totalSpore++;
+        //setApprovalForAll(stakeContract, true);  // need edit
+    }
+
+    function SporeToMush(uint _tokenId, uint256 _tokenRate, string memory _tokenuri) public onlyOwner {
+        require(NFTinfo[_tokenId].staking == false, "not Spore");
+
+        NFTinfo[_tokenId].staking = true;
+        NFTinfo[_tokenId].tokenRate = _tokenRate;
+        setTokenUri(_tokenId, _tokenuri, 0);
+        totalMush++;
+        totalSpore--;
+    }
+
+    function MushToSpore(uint _tokenId, uint256 _tokenRate, string memory _tokenuri) public onlyOwner {
+        require(NFTinfo[_tokenId].staking == true, "not Mushroom");
+
+        NFTinfo[_tokenId].staking = false;
+        NFTinfo[_tokenId].tokenRate = _tokenRate;
+        setTokenUri(_tokenId, _tokenuri, 0);
+        totalMush--;
+        totalSpore++;
     }
 
     // need onlyowner
-    function mintNFT(address _account, uint _tokenId, string memory _tokenuri, uint256 _tokenRate) public {
+    function mintMush(address _account, uint _tokenId, string memory _tokenuri, uint256 _tokenRate) public {
+        require(balanceOf(_account, _tokenId) > 0, "Not owned token");
+        _burn(_account, _tokenId, 1);
+
         _mint(_account, _tokenId, 1, "");
         setTokenUri(_tokenId, _tokenuri, 0);
-        tokenRate[_tokenId] = _tokenRate;
+
+        NFTinfo[_tokenId].tokenRate = _tokenRate;
+        NFTinfo[_tokenId].staking = true;
+
         season[_tokenId] = currentSeason;
+        totalMush++;
+        //setApprovalForAll(stakeContract, true);  // need edit
+    }
+
+
+    function mintStealer(address _account, uint _tokenId, string memory _tokenuri) public {
+        require(balanceOf(_account, 0) > 1000, "Not enough Main token");  // 1000 is price
+        _burn(_account, 0, 1000);
+
+        _mint(_account, _tokenId, 1, "");
+        setTokenUri(_tokenId, _tokenuri, 0);
+        stealer[_tokenId] = 1;  // true 의미, 혹시 몰라서 일단 정수로 설정
+        //season[_tokenId] = currentSeason;
         //setApprovalForAll(stakeContract, true);  // need edit
     }
 
     function viewTokenRate(uint256 _tokenId) public returns(uint256){
-        return tokenRate[_tokenId];
+        return NFTinfo[_tokenId].tokenRate;
     }
 
     function viewSeason(uint256 _tokenId) public returns(uint256){
@@ -68,5 +126,14 @@ contract token is ERC1155 {
 
     function updateSeason() public onlyOwner{
         currentSeason++;
+    }
+
+    function checkStealer(uint _stealtokenId) public view returns(bool){
+        return stealer[_stealtokenId] == 1;
+    }
+
+    function burn(address _account, uint _tokenId, uint _amount) public {
+        _burn(_account, _tokenId, _amount);
+
     }
 }
